841b03dd2625376462465ee7600700a8735e90a0
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-04 22:42:19 +0600

    preview mode function created

---
+++ utils/sanity.preview.ts
@@ -0,0 +1,16 @@
+"use client"
+import { definePreview } from 'next-sanity/preview';
+import { projectId, dataset } from './sanity.client';
+
+function onPublicAccessOnly() {
+    throw new Error("Unable to load preview as you are not logged in")
+}
+if (!projectId || !dataset) {
+    throw new Error("Project Id or dataset not found. Check your sanity file or .env")
+}
+
+export const usePreview = definePreview({
+    projectId,
+    dataset,
+    onPublicAccessOnly,
+})
