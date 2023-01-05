ce90a341dc9fde45523dc67cd8a39127a079fa04
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-05 16:46:38 +0600

    type def created

---
+++ components/BlogList.tsx
@@ -0,0 +1,14 @@
+import { Post } from "../typing";
+
+type Props = {
+    posts:Post[]
+}
+const BlogList = ({posts}:Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BlogList;
