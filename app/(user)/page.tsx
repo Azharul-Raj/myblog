b1f46325d5756b4f5ad66dd68025876dbaa939af
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-05 15:20:04 +0600

    preview mode function created

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,4 +1,14 @@
-// import React from 'react'
+
+import { groq } from "next-sanity"
+
+const query = groq`
+*[_type=='post']{
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt desc)
+
+`
 
 export default function HomePage() {
   return (
