6adf03d1326a2317767fe117081f821ab04fa57f
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-06 20:24:00 +0600

    blog details part disinging

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { groq } from "next-sanity"
+import Image from "next/image"
 import { client } from "../../../../lib/sanity.client"
+import urlFor from "../../../../lib/urlFor"
 import { Post } from "../../../../typing"
 
 type Props = {
@@ -7,19 +9,50 @@ type Props = {
         slug:string
     }
 }
+
 async function Post({ params: { slug } }: Props) {
-    const query = groq`
-    *[type='post' && slug.current=$slug][0]
+
+      const query = groq`
+    *[_type=='post' && slug.current==$slug][0]
     {
         ...,
         author->,
         categories[]->
     }
     `
-    const post: Post = await client.fetch(query, { slug });
+  const post: Post = await client.fetch(query, { slug });
   return (
-    <div>page</div>
+    <article className="px-10 pb-28">
+      <section className="space-y-2 border border-yellow-300 text-white">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 h-full w-full  opacity-10 blur-sm p-10">
+          <Image className="object-cover object-left lg:object-center"
+                                src={urlFor(post.mainImage).url()}
+                                alt={post.author.name}
+                                fill
+                            />
+          </div>
+          <section className="p-5 bg-gray-400 w-full">
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div className="">
+                <h1 className="text-4xl font-extrabold">{ post?.title}</h1>
+                <p>
+                                        {new Date(post?._createdAt).toLocaleDateString(
+                                            'en-US', {
+                                                day: 'numeric',
+                                                month: 'long',
+                                                year:'numeric'
+                                            }
+                                        )}
+                                    </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </article>
   )
+    
 }
 
 export default Post;
